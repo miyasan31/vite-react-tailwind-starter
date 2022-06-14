2c7a862f8856e5076ea1b364528541df07a48010
miyasan_0301 <miyasan.19990301@gmail.com>
2022-06-15 08:42:20 +0900

    🔧

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,7 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./vitest.setup.ts",
+    include: ["src/**/*.test.{ts,tsx}"],
+    exclude: ["**/node_modules/**", "**/dist/**", "e2e/**"],
   },
 });
