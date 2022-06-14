4a858c97c8ae56234633f796729bf8fdfabd5f7e
miyasan_0301 <miyasan.19990301@gmail.com>
2022-06-14 23:40:34 +0900

    ♿️ text contrast

--- src/constants/buttonColor.ts
+++ src/constants/buttonColor.ts
@@ -1,2 +1,2 @@
-export const successButton = "bg-green-600 text-white font-bold py-2 px-4";
-export const errorButton = "bg-red-500 text-white font-bold py-2 px-4";
+export const successButton = "bg-blue-600 text-white font-bold py-2 px-4";
+export const errorButton = "bg-rose-600 text-white font-bold py-2 px-4";
