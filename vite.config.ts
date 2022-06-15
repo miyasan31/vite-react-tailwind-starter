430d063a82aff26337a1117acbcf5453bb5f998d
miyasan_0301 <miyasan.19990301@gmail.com>
2022-06-15 09:10:25 +0900

    🔧 PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,41 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import type { VitePWAOptions } from "vite-plugin-pwa";
 import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const pwaOptions: Partial<VitePWAOptions> = {
+  base: "src/assets/",
+  includeAssets: ["favicon.svg"],
+  manifest: {
+    name: "PWA",
+    short_name: "PWA",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "pwa-192x192.png", // <== don't add slash, for testing
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/pwa-512x512.png", // <== don't remove slash, for testing
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512x512.png", // <== don't add slash, for testing
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+  },
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), VitePWA({})],
+  plugins: [react(), tsconfigPaths(), VitePWA(pwaOptions)],
   assetsInclude: ["robots.txt"],
   test: {
     globals: true,
